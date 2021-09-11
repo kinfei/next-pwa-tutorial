05c3a0c1d093f1e1f020b92ed83b39bfd4e87b94
Lee Kin Fei <kinfei1230@gmail.com>
2021-09-11 13:28:15 +0800

    Add fixes

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,9 +1,3 @@
-function MyApp() {
-  return (
-    <>
-      <div>You are offline. Please connect to the internet.</div>
-    </>
-  );
+export default function Offline() {
+  return <div>You are offline. Please connect to the internet.</div>;
 }
-
-export default MyApp;
